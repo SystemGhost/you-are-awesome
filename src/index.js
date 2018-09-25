a06844af9ccee8f685423d2dd0da126c45e55a5c
SystemGhost <jackie07364538@gmail.com>
2018-09-25 15:23:17 +0300

    task/feature solved 5 fo 10

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
+const createEnumerableProperty = (propertyName) => propertyName;
+const createNotEnumerableProperty = (propertyName) => Symbol(propertyName);
 const createProtoMagicObject = () => {};
-const incrementor = () => {};
+
+let i = 0;
+const incrementor = () => {
+	i++;
+	return incrementor;
+};
+incrementor.valueOf = function() {
+	return i;
+}
 const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createIncrementer = function*() {
+	let index = 0;
+	while (index != 'undefined') yield ++index;
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (param) => {
+
+
+};
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (array) => {
+	array.sort((a, b) => a.__proto__ - b.__proto__);
+	return array;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
